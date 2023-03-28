7ffdd485ad7865c6c10378245be804c6e7c7d760
pi nuke <smartguy1196@gmail.com>
2023-03-28 13:01:07 -0700

    feat(AsyncRuntime): :sparkles: add winform, wpf, and avalonia runtimes

---
+++ pwsh/initializers/avalonia.ps1
@@ -0,0 +1,27 @@
+Initialize-Runtime -Name "Avalonia" -Script {
+    $Builder = [Avalonia.AppBuilder]::Configure[Avalonia.Application]()
+    $Builder = [Avalonia.AppBuilderDesktopExtensions]::UsePlatformDetect( $Builder )
+
+    $Lifetime = [Avalonia.Controls.ApplicationLifetimes.ClassicDesktopStyleApplicationLifetime]::new()
+    $Lifetime.ShutdownMode = [Avalonia.Controls.ShutdownMode]::OnExplicitShutdown
+
+    $Builder = $Builder.SetupWithLifetime( $Lifetime )
+
+    $App = $Appbuilder.Instance
+
+    Invoke-Command $PostDispatcher -ArgumentList @( [Avalonia.Threading.Dispatcher]::UIThread )
+
+    $Themes = @{
+        "Default" = [Avalonia.Markup.Xaml.Styling.StyleInclude]::new( [System.Uri] $null )
+        "BaseLight" = [Avalonia.Markup.Xaml.Styling.StyleInclude]::new( [System.Uri] $null )
+    }
+    $Themes.Default.Source = [System.Uri]::new( "avares://Avalonia.Themes.Default/DefaultTheme.xaml" )
+    $Themes.BaseLight.Source = [System.Uri]::new( "avares://Avalonia.Themes.Default/Accents/BaseLight.xaml" )
+
+    $App.Styles.Add( $Themes.Default )
+    $App.Styles.Add( $Themes.BaseLight )
+
+    $CTS = [System.Threading.CancellationTokenSource]::new()
+
+    [Avalonia.Controls.DesktopApplicationExtensions]::Run( $App, $CTS.Token )
+} | Out-Null
--- pwsh/main.ps1
+++ pwsh/main.ps1
@@ -15,3 +15,7 @@ $LoadArgs = @{
 & "$Root/git/PowerLoader/pwsh/load.ps1" @LoadArgs
 
 & "$Root/pwsh/threading/runtimes.ps1"
+
+& "$Root/pwsh/initializers/winform.ps1"
+& "$Root/pwsh/initializers/wpf.ps1"
+& "$Root/pwsh/initializers/avalonia.ps1"
