28f1ac21f5f96a60f6e71d3d7eabdefdc567ac94
pi nuke <smartguy1196@gmail.com>
2023-04-01 22:53:55 -0700

    feat(AsyncRuntime): :sparkles: add option to call script file as a delegate

--- pwsh/threading/delegate.ps1
+++ pwsh/threading/delegate.ps1
@@ -1,13 +1,22 @@
 function global:Invoke-Delegate{
     param(
         [Parameter(Mandatory=$true)]
-        [scriptblock] $Delegate,
-        [Parameter(Mandatory=$true)]
         [string] $Runtime,
+        [scriptblock] $Delegate,
+        [string] $Path,
         [switch] $Sync
     )
 
-    $Delegate = [scriptblock]::Create( $Delegate.ToString() )
+    if ( !$Path -and !$Delegate ) {
+        throw [System.ArgumentException]::new('Either Path or Script must be specified', 'Delegate')
+    }
+
+    If ( $Path ) {
+        $Delegate = Import-Contents -Path $Path -As ScriptBlock
+    } else {
+        $Delegate = [scriptblock]::Create( $Delegate.ToString() )
+    }
+    
     $Task = $Runtimes[ $Runtime ].Dispatcher.InvokeAsync( [System.Action]$Delegate )
 
     if ( $Sync ) {
